821de35c6bf6a8c8a4f855115f298f624a7eb00b
Ban Lee <53801519+ban-lee@users.noreply.github.com>
2022-09-21 12:10:40 -0400

    Redirect from the root to the special basePath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: false,
   basePath: '/ffxiv-island-sanctuary',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/ffxiv-island-sanctuary',
+        permanent: false,
+        basePath: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
