5339abd9e4702cde3ba583026a61ba28f3e33390
Ban Lee <53801519+ban-lee@users.noreply.github.com>
2022-09-26 14:11:05 -0400

    This time for sure. Calculate last weekly reset correctly

--- utils/season-util.ts
+++ utils/season-util.ts
@@ -1,16 +1,18 @@
 import dayjs from 'dayjs';
-import isoWeek from 'dayjs/plugin/isoWeek';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import utc from 'dayjs/plugin/utc';
 
-dayjs.extend(isoWeek);
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
 
 export function calculateCycleInfo(): {cycle: number, timeRemaining: string} {
   const today = dayjs().utc();
-  const nextCycleStart = today.startOf('day').add(today.hour() >= 8 ? 1 : 0, 'day').add(8, 'hour');
-  const seasonStart = today.startOf('isoWeek').add(2, 'day').add(8, 'hour');
+  const nextCycleStart = today.startOf('day').add(today.hour() >= 8 ? 1 : 0, 'day').hour(8);
+
+  let seasonStart = today.startOf('week').day(2).hour(8);
+  if (today.isBefore(seasonStart)) {
+    seasonStart = seasonStart.subtract(7, 'day');
+  }
 
   return {
     cycle: today.diff(seasonStart, 'day') + 1,
