ae99589506671025fcd1a20898db816ee4c97e94
Ban Lee <53801519+ban-lee@users.noreply.github.com>
2022-09-28 12:27:09 -0400

    Deploy web app to GitHub Pages

---
+++ out/_next/static/Do9ZABJoiM9u4A0Y1nbwo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-fbcfe2172188e46f.js","static/chunks/842-a95a5b036412ac2b.js","static/chunks/pages/index-5a9ff2d114e0a249.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
